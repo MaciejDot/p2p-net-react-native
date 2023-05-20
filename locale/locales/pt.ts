f6462a057f3e25de911b901f830d2de63ae67251
Maciek_dot <maciejd0@op.pl>
2023-05-20 13:27:35 +0200

    internationalization

---
+++ locale/locales/pt.ts
@@ -0,0 +1,38 @@
+import { PartialLocaleConfig } from './LocaleConfig'
+
+const pt: PartialLocaleConfig = {
+    'Log In': 'Entrar',
+    Chats: 'Conversas',
+    'Your Info': 'Suas informações',
+    Ping: 'Ping',
+    Statistics: 'Estatísticas',
+    'There arent any active chats': 'Não há conversas ativas',
+    'To start the first one click on the plus icon':
+        'Para iniciar a primeira, clique no ícone de adição',
+    'Create new account': 'Criar nova conta',
+    'There was an error loging in...': 'Ocorreu um erro ao entrar...',
+    password: 'senha',
+    username: 'nome de usuário',
+    'Login instead': 'Entrar em vez disso',
+    Register: 'Registrar',
+    'There was an error registering...': 'Ocorreu um erro ao registrar...',
+    'repeat password': 'repetir senha',
+    'Repeat password and password should be the same':
+        'A senha e a repetição da senha devem ser iguais',
+    'Your ping address': 'Seu endereço de ping',
+    copy: 'copiar',
+    'copied to clipboard': 'copiado para a área de transferência',
+    'remote ping address': 'endereço de ping remoto',
+    'start pinging': 'iniciar o ping',
+    'invalid address': 'endereço inválido',
+    'stop pinging': 'parar o ping',
+    time: 'tempo',
+    status: 'status',
+    success: 'sucesso',
+    'not a success': 'não é um sucesso',
+    'Connected peers': 'Usuários conectados',
+    'Connected servers': 'Servidores conectados',
+    Ip: 'Ip',
+    Port: 'Porta',
+}
+export default pt
